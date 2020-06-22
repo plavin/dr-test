14196ee8ab39f62449b942b7581a0a01879a9627
Patrick Lavin <plavin997@gmail.com>
2020-06-22 15:32:15 -0500

    add log with repeating phases

--- test.c
+++ test.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
-#define len 10
+#define len 10000
 int main(int argc, char **argv)
 {
     int *arr = (int*)malloc(sizeof(int) * len);
 
-    for (int i = 0; i < len; i++) {
-       arr[i] = i*i;
-    }
+    for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < len; i++) {
+           arr[i] = i*i;
+        }
 
-    for (int i = 0; i < len; i++) {
-       arr[i] = i/3;
-    }
+        for (int i = 0; i < len; i++) {
+           arr[i] = i/3;
+        }
 
-    if (arr[0]) {
-        exit(1);
+        if (arr[0]) {
+            exit(1);
+        }
     }
     exit(0);
 }
